866911c7a50e4cf7cc2b8bfac3bc1fe4ea039f65
Tomas Mogensen <teemodk@gmail.com>
2016-12-11 00:22:35 +0100

    Revert "Disable mouse pointer" Worked, but with sideeffects. Flimsy display, like when lis3dh-accel is not disabled.
    This reverts commit f946ae867246a2983388603ee4a4f58555a0b8dc.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,6 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_IGNORE_ABS_MT_TRACKING_ID := true
 TW_INCLUDE_CRYPTO := true
 TW_INPUT_BLACKLIST := "lis3dh-accel"
-TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_NO_USB_STORAGE := true
 TW_THEME := portrait_hdpi
 
